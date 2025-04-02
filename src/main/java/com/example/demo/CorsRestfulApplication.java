a53c943834a1fea28435de42cfd7af69dfe2cf9d
ndys <hendy.sg02@gmail.com>
2025-04-02 21:30:37 +0700

    [feat] project init

---
+++ src/main/java/com/example/demo/CorsRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsRestfulApplication.class, args);
+	}
+
+}
